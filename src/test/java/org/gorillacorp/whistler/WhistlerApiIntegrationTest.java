8d55efda6b40e81aee96ac9d6a0559a4e8505635
danikun <steelturtle@msn.com>
2020-12-13 17:53:02 +0900

    Initial integration testing for User entity [WIP]

--- src/test/java/org/gorillacorp/whistler/WhistlerApiIntegrationTest.java
+++ src/test/java/org/gorillacorp/whistler/WhistlerApiIntegrationTest.java
@@ -1,9 +1,9 @@
 package org.gorillacorp.whistler;
 
-import jdk.jfr.Description;
 import org.gorillacorp.whistler.domain.model.User;
 import org.gorillacorp.whistler.domain.repository.UserRepository;
 import org.gorillacorp.whistler.domain.repository.WhistleRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,7 +18,7 @@ public class WhistlerApiIntegrationTest {
     private WhistleRepository whistleRepository;
 
     @Test
-    @Description("Users can create and save their own whistles in the DB")
+    @DisplayName("Users can create and save their own whistles in the DB")
     void usersCanSaveTheirOwnWhistles() {
         var user = userRepository.save(new User("dummy_user1"));
         assertNotNull(user);
